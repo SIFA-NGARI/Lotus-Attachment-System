88aab478b6bd9c97affd27199adc2aa272f96c14
Olive Menorah <you@example.com>
2023-07-14 03:40:42 +0300

    student dashboard changes

--- resources/views/student_dashboard.blade.php
+++ resources/views/student_dashboard.blade.php
@@ -435,12 +435,12 @@
                         <!-- right col (We are only adding the ID to make the widgets sortable)-->
                         <section class="col-lg-5 connectedSortable">
 
-                            <!-- Map card -->
+                            <!-- MAP card -->
                             <div class="card bg-gradient-primary">
                                 <div class="card-header border-0">
                                     <h3 class="card-title">
                                         <i class="fas fa-map-marker-alt mr-1"></i>
-                                        Visitors
+                                        Map location
                                     </h3>
                                     <!-- card tools -->
                                     <div class="card-tools">
@@ -459,20 +459,7 @@
                                 <!-- /.card-body-->
                                 <div class="card-footer bg-transparent">
                                     <div class="row">
-                                        <div class="col-4 text-center">
-                                            <div id="sparkline-1"></div>
-                                            <div class="text-white">Visitors</div>
-                                        </div>
-                                        <!-- ./col -->
-                                        <div class="col-4 text-center">
-                                            <div id="sparkline-2"></div>
-                                            <div class="text-white">Online</div>
-                                        </div>
-                                        <!-- ./col -->
-                                        <div class="col-4 text-center">
-                                            <div id="sparkline-3"></div>
-                                            <div class="text-white">Sales</div>
-                                        </div>
+    
                                         <!-- ./col -->
                                     </div>
                                     <!-- /.row -->
